1bf36ef75b6884a082f2e205479c41ae644674e7
LifelongYuan <zhb_00@126.com>
2021-04-16 11:09:03 +0800

    Done with Big environment in environment.py. 3 TODO depends on matlab format

--- bindsnet/environment/environment.py
+++ bindsnet/environment/environment.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, Dict, Any
+from typing import Tuple, Dict, Any,Optional,Callable
 
 import gym
 import numpy as np
 import torch
 from ..datasets.preprocess import subsample, gray_scale, binary_image, crop
 from ..encoding import Encoder, NullEncoder
-#import matlab.engine
+import matlab.engine
 
 class Environment(ABC):
     # language=rst
@@ -126,6 +126,7 @@ class GymEnvironment(Environment):
         :return: Observation, reward, done flag, and information dictionary.
         """
         # Call gym's environment step function.
+        # Call external engine step function, take action
         self.obs, self.reward, self.done, info = self.env.step(a)
 
         if self.clip_rewards:
@@ -263,195 +264,101 @@ class MuscleEnvironment(Environment):
     A wrapper around the OpenAI ``gym`` environments.
     """
 
-    def __init__(self, name: str, encoder: Encoder = NullEncoder(), **kwargs) -> None:
+    def __init__(self,
+                 n_mat_step:int,
+                 MATLABSTEPTIME:float,
+                 **kwargs) -> None:
         # language=rst
         """
-        Initializes the environment wrapper. This class makes the
-        assumption that the OpenAI ``gym`` environment will provide an image
-        of format HxW or CxHxW as an observation (we will add the C
-        dimension to HxW tensors) or a 1D observation in which case no
-        dimensions will be added.
+           :param n_mat_step: one step network run ,n_mat_step eng run
+           :param MATLABSTEPTIME: eng time per step
 
-        :param name: The name of an OpenAI ``gym`` environment.
-        :param encoder: Function to encode observations into spike trains.
-
-        Keyword arguments:
-
-        :param float max_prob: Maximum spiking probability.
-        :param bool clip_rewards: Whether or not to use ``np.sign`` of rewards.
-
-        :param int history: Number of observations to keep track of.
-        :param int delta: Step size to save observations in history.
-        :param bool add_channel_dim: Allows for the adding of the channel dimension in
-            2D inputs.
         """
-        self.name = name
-        # TODO
+
         matlab.engine.start_matlab()  # start the topic from matlab
         self.eng = matlab.engine.connect_matlab()  # connect the topic
-        assert (self.eng is None), "Failed to connect with  matlab" # if not, exit
-        self.eng.load_system('actuator.slx')  # load the model
-        self.Joint_angle = []
-        #
-        # self.encoder = encoder
-        #
-        # # Keyword arguments.
-        # self.max_prob = kwargs.get("max_prob", 1.0)
-        # self.clip_rewards = kwargs.get("clip_rewards", True)
-        #
-        # self.history_length = kwargs.get("history_length", None)
-        # self.delta = kwargs.get("delta", 1)
-        # self.add_channel_dim = kwargs.get("add_channel_dim", True)
-        #
-        # if self.history_length is not None and self.delta is not None:
-        #     self.history = {
-        #         i: torch.Tensor()
-        #         for i in range(1, self.history_length * self.delta + 1, self.delta)
-        #     }
-        # else:
-        #     self.history = {}
-        #
-        # self.episode_step_count = 0
-        # self.history_index = 1
-        #
-        # self.obs = None
-        # self.reward = None
-        # self.state = None;
-        # assert (
-        #     0.0 < self.max_prob <= 1.0
-        # ), "Maximum spiking probability must be in (0, 1]."
+        assert (self.eng is not None), "Failed to connect with  matlab"  # if not, exit
+        self.n_mat_step = n_mat_step;
+        self.MATLABSTEPTIME = MATLABSTEPTIME;
+        self.Info_muscle = {"Transmitter": 0, "current_command": [0, 0]}
+        self.sim_name = None
 
-    def step(self, a: int) -> Tuple[torch.Tensor, float, bool, Dict[Any, Any]]:
+    def start(self,sim_name:str='actuator.slx'):
+        # language=rst
+        """
+            start the Simulink
+           :param sim_name: the name of simulink file prepared to run
+
+        """
+        self.sim_name = sim_name
+        self.eng.load_system(sim_name)  # load the model
+        print("-"*10+"Simulink start"+"-"*10)
+
+    def muscle_step(self,
+             record_list:list,
+             command_list:list) -> None:
         # language=rst
         """
            simulate a single step and record output from simulink
+           :param record_list: names of the variable in eng you want to record into Info_muscle,every name must be a string type
+
         """
-        # Call simulink's environment to step
-        self.eng.set_param("actuator", "SimulationCommand", "start", nargout=0)
-        self.eng("actuator", "SimulationCommand", "step", nargout=0)
-        self.eng("actuator", "SimulationCommand", "pause", nargout=0)
+        # Send command to eng
+        self.Send_control(command_list)
+        # Call eng environment to run for n_mat_step
+        for i in range(self.n_mat_step):
+            self.eng.set_param(self.sim_name, "SimulationCommand", "start", nargout=0)
+            self.eng(self.sim_name, "SimulationCommand", "step", nargout=0)
+            self.eng(self.sim_name, "SimulationCommand", "pause", nargout=0)
+        # load data from eng to Info
+        self.Rec_eng_Info([record_list])
+
+    def Rec_eng_Info(self,para_list:list)->None:
         # TODO due to the data structure of matlab workspace
-        self.Joint_angle.append(self.eng.workspace["angle1"])
-        self.preprocess()
-
-        # Add the raw observation from the gym environment into the info
-        # for debugging and display.
-        info["gym_obs"] = self.obs
-
-        # Store frame of history and encode the inputs.
-        if len(self.history) > 0:
-            self.update_history()
-            self.update_index()
-            # Add the delta observation into the info for debugging and display.
-            info["delta_obs"] = self.obs
-
-        # The new standard for images is BxTxCxHxW.
-        # The gym environment doesn't follow exactly the same protocol.
-        #
-        # 1D observations will be left as is before the encoder and will become BxTxL.
-        # 2D observations are assumed to be mono images will become BxTx1xHxW
-        # 3D observations will become BxTxCxHxW
-        if self.obs.dim() == 2 and self.add_channel_dim:
-            # We want CxHxW, it is currently HxW.
-            self.obs = self.obs.unsqueeze(0)
-
-        # The encoder will add time - now Tx...
-        if self.encoder is not None:
-            self.obs = self.encoder(self.obs)
-
-        # Add the batch - now BxTx...
-        self.obs = self.obs.unsqueeze(0)
-
-        self.episode_step_count += 1
-
-        # Return observed Joint_angle
-        return self.Joint_angle
-
-    def reset(self) -> torch.Tensor:
+        # TODO deal with non-exist para_name
         # language=rst
         """
-        Wrapper around the OpenAI ``gym`` environment ``reset()`` function.
-
-        :return: Observation from the environment.
+            load desired eng variable from workspace to "Info_muscle"
+           :param para_list: name list of the eng variable you want to record
         """
-        # Call gym's environment reset function.
-        self.obs = self.env.reset()
-        self.preprocess()
+        if len(para_list) is 0:
+            print("You want to record empty!")
+        else:
+            for l in para_list:
+                assert(isinstance(l,str),"Invaild record key! Key must be string type")
+                self.Info_muscle[l] = self.eng.workspace[l]
 
-        self.history = {i: torch.Tensor() for i in self.history}
-
-        self.episode_step_count = 0
-
-        return self.obs
-
-    def render(self) -> None:
+    def Send_control(self,command_list:list):
+        # TODO due to the data structure of matlab workspace
         # language=rst
         """
-        Wrapper around the OpenAI ``gym`` environment ``render()`` function.
+            load desired eng variable from workspace to "Info_muscle"
+           :param command_list: name list of the variable you want to send from Info_muscle to eng
         """
-        self.env.render()
+        if len(command_list) is 0:
+            print("You want to record empty!")
+        else:
+            for c in command_list:
+                assert(isinstance(c,str),"Invaild command key! Key must be string type")
+                assert(self.Info_muscle.get(c) is not None,"No such key in Info_muscle")
+                self.eng.workspace[c] = self.Info_muscle[c]
+                self.eng.workspace[c] = self.Info_muscle[c]
+
+    def reset(self) -> None:
+        # language=rst
+        """
+        reset eng and clear the Info dictionary
+        """
+        self.eng.reset() #TODO matlab reload model
+        self.Info_muscle = {}
 
     def close(self) -> None:
         # language=rst
         """
         Wrapper around the OpenAI ``gym`` environment ``close()`` function.
         """
-        self.env.close()
+        assert(self.sim_name is not None,"No simulink is running!")
+        self.eng(self.sim_name, "SimulationCommand", "stop", nargout=0)
 
-    def preprocess(self) -> None:
-        # language=rst
-        """
-        Pre-processing step for an observation from a ``gym`` environment.
-        """
-        if self.name == "SpaceInvaders-v0":
-            self.obs = subsample(gray_scale(self.obs), 84, 110)
-            self.obs = self.obs[26:104, :]
-            self.obs = binary_image(self.obs)
-        elif self.name == "BreakoutDeterministic-v4":
-            self.obs = subsample(gray_scale(crop(self.obs, 34, 194, 0, 160)), 80, 80)
-            self.obs = binary_image(self.obs)
-        else:  # Default pre-processing step.
-            pass
 
-        self.obs = torch.from_numpy(self.obs).float()
 
-    def update_history(self) -> None:
-        # language=rst
-        """
-        Updates the observations inside history by performing subtraction from most
-        recent observation and the sum of previous observations. If there are not enough
-        observations to take a difference from, simply store the observation without any
-        differencing.
-        """
-        # Recording initial observations.
-        if self.episode_step_count < len(self.history) * self.delta:
-            # Store observation based on delta value.
-            if self.episode_step_count % self.delta == 0:
-                self.history[self.history_index] = self.obs
-        else:
-            # Take difference between stored frames and current frame.
-            temp = torch.clamp(self.obs - sum(self.history.values()), 0, 1)
-
-            # Store observation based on delta value.
-            if self.episode_step_count % self.delta == 0:
-                self.history[self.history_index] = self.obs
-
-            assert (
-                len(self.history) == self.history_length
-            ), "History size is out of bounds"
-            self.obs = temp
-
-    def update_index(self) -> None:
-        # language=rst
-        """
-        Updates the index to keep track of history. For example: ``history = 4``,
-        ``delta = 3`` will produce ``self.history = {1, 4, 7, 10}`` and
-        ``self.history_index`` will be updated according to ``self.delta`` and will wrap
-        around the history dictionary.
-        """
-        if self.episode_step_count % self.delta == 0:
-            if self.history_index != max(self.history.keys()):
-                self.history_index += self.delta
-            else:
-                # Wrap around the history.
-                self.history_index = (self.history_index % max(self.history.keys())) + 1
